637184e8450601ebe3b0919a1fa1204381e3cfaa
Taylon kalel <taylon.kalel@gmail.com>
2024-08-17 00:26:07 -0300

    melhoria do widget button

--- lib/pages/home/home.mobile.widget.dart
+++ lib/pages/home/home.mobile.widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter324/widget/button.widget.dart';
 import 'package:flutter324/widget/text.widget.dart';
 
 class HomeMobileWidget extends StatelessWidget {
@@ -9,8 +10,14 @@ class HomeMobileWidget extends StatelessWidget {
     return Container(
       width: MediaQuery.of(context).size.width,
       alignment: Alignment.center,
-      child: const TextWidget(
-        text: "MOBILE",
+      child: Column(
+        children: [
+          const TextWidget(
+            text: "MOBILE",
+          ),
+          ButtonWidget(
+              onPressed: () {}, child: const TextWidget(text: "Botão"))
+        ],
       ),
     );
   }
--- lib/widget/button.widget.dart
+++ lib/widget/button.widget.dart
@@ -3,26 +3,24 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-class Button extends StatelessWidget {
-  const Button({super.key});
+class ButtonWidget extends StatelessWidget {
+  final Function()? onPressed;
+  final Widget child;
+  const ButtonWidget({super.key, required this.onPressed, required this.child});
 
   @override
   Widget build(BuildContext context) {
     if (Platform.isIOS) {
       return CupertinoButton(
         color: CupertinoColors.activeBlue,
-        child: const Text('Cupertino Button'),
-        onPressed: () {
-          print('Cupertino button pressed');
-        },
+        onPressed: onPressed,
+        child:child,
       );
     } else {
       return ElevatedButton(
         style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-        child: Text('Material Button'),
-        onPressed: () {
-          print('Material button pressed');
-        },
+        onPressed: onPressed,
+        child:child,
       );
     }
   }
